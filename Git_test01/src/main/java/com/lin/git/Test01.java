8896358773d9ab77ae9376802b1e5544d12328cf
Lins <test@Lins.com>
2021-10-28 15:48:57 +0800

    second modification

--- Git_test01/src/main/java/com/lin/git/Test01.java
+++ Git_test01/src/main/java/com/lin/git/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("create a Repository");
         System.out.println("first modification");
+        System.out.println("second modification");
     }
 }
