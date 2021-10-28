2af16b69b47dece6f82404c6d24bc22c8ce10710
Lins <test@Lins.com>
2021-10-28 15:48:18 +0800

    first modification

--- Git_test01/src/main/java/com/lin/git/Test01.java
+++ Git_test01/src/main/java/com/lin/git/Test01.java
@@ -3,5 +3,6 @@ package com.lin.git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("create a Repository");
+        System.out.println("first modification");
     }
 }
